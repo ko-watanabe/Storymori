93473175e00cc9cacd4e7c4763212685a615b257
Ko Watanabe <ko.watanabe@dena.jp>
2019-05-24 10:21:15 +0900

    MODIFY controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,25 @@
 class PagesController < ApplicationController
   
   def get
-    
+    page = Page.find_by!(id: params[:pageId], story_id: params[:storyId])
+    entity = page.attributes
+    entity[:children] = page.children
+    render json: entity
+  rescue ActiveRecord::RecordNotFound => e
+      render status: 404, json: { errors: exception }
   end
 
   def save
+    page = Page.new(name: params[:name], text: params[:text], story_id: params[:storyId], parent_id: params[:parentId])
+    if page.save
+      render status: 201, json: { id: page.id }
+    else
+      render status: 422, json: { errors: page.errors }
+    end
+  end
 
+  def notfound
+    render html: helpers.tag.strong("No route exists! Sorry!")
   end
 
 end
--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,12 +1,12 @@
 class StoriesController < ApplicationController
   
   def get
-    stories = Stories.all()
+    stories = Story.joins(:pages).select("stories.id, stories.title, pages.id, As parantId").where("pages.parent_id IS NULL")
     render json: stories
   end
 
   def save
-    story = Stories.create(title: params[:title], summary: params[:text].slice(0, 10))
+    story = Story.create(title: params[:title], summary: params[:text].slice(0, 10))
     page = Pages.create(name: params[:title], text: params[:text], story_id: story.id)
     render json: { story_id: story.id, page_id: page.id }
   end
