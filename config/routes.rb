94f7e31dfa5406e85abf458b523ddb588b56c114
Ko Watanabe <ko.watanabe@dena.jp>
2019-05-24 10:22:37 +0900

    MODIFY routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'api/stories', to: 'stories#get'
-  post 'api/stories', to: 'stories#save'
-  get 'api/stories/:storyId/pages/:pageId', to: 'pages#get'
-  post 'api/stories/:storyId/pages/:parentId:/next', to: 'pages#save'
-  get '*path' => 'stories#notfound'
+  namespace :api, default: { format: :json } do
+    get '/stories', to: 'stories#get'
+    post '/stories', to: 'stories#save'
+    get '/stories/:storyId/pages/:pageId', to: 'pages#get'
+    post '/stories/:storyId/pages/:parentId:/next', to: 'pages#save'
+    get '*path' => 'stories#notfound'
+  end
 end
